081ca5aa91b3fa100d4c6a87278f147d9354bc1b
boshra-irnd <boshra.irnd@gmail.com>
2022-03-13 03:42:45 -0700

    start project

---
+++ jobsin/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for jobsin project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'jobsin.settings.common')
+
+application = get_asgi_application()
